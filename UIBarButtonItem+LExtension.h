07b61ab4bb51eae1818fdc87714ddbdc23f7e3bb
liaoshe <416862100@qq.com>
2017-05-17 09:55:12 +0800

    更新barButtonItem

--- UIBarButtonItem+LExtension.h
+++ UIBarButtonItem+LExtension.h
@@ -3,19 +3,63 @@
 //  UIBarButtonItem+LExtension
 //
 //  Created by Jimmy_Lee on 2016/12/19.
-//  Copyright © 2016年 jimmy_lee. All rights reserved.
+//  Copyright © 2016年 Jimmy_Lee. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
 @interface UIBarButtonItem (LExtension)
-/**
- * 创建带图片的barButtonItem
- */
-+ (instancetype)itemWithImage:(NSString *)image HeightImage:(NSString *)heightImage Target:(id)target action:(SEL)action;
 
 /**
- * 创建只有文字的barButtonItem，并且可以设置文字颜色
+ 创建带有图片的barButtonItem
+ 
+ @param image       默认图片
+ @param selectImage 选中图片
+ @param target      对象
+ @param action      点击事件
+ @return            创建实例对象
  */
-+ (instancetype)itemWithTitle:(NSString *)title TitleColor:(UIColor *)titleColor TitleFont:(CGFloat)titleFont Target:(id)target action:(SEL)action;
++ (instancetype)itemWithImage:(NSString *)image
+                  selectImage:(NSString *)selectImage
+                       target:(id)target
+                       action:(SEL)action;
+
+
+
+/**
+ 创建只有文字的barButtonItem，并且可以设置文字颜色
+
+ @param title       标题
+ @param titleColor  标题颜色
+ @param titleFont   标题字体大小
+ @param target      对象
+ @param action      点击事件
+ @return            创建实例对象
+ */
++ (instancetype)itemWithTitle:(NSString *)title
+                   titleColor:(UIColor *)titleColor
+                    titleFont:(CGFloat)titleFont
+                       target:(id)target
+                       action:(SEL)action;
+
+
+/**
+ 创建带有图片和文字的barButtonItem
+ 
+ @param image       默认图片
+ @param selectImage 选中时的图片
+ @param title       标题
+ @param titleColor  标题颜色
+ @param titleFont   标题字体大小
+ @param target      对象
+ @param action      点击事件
+ @return            创建实例对象
+ */
++ (instancetype)itemWithImage:(NSString *)image
+                  selectImage:(NSString *)selectImage
+                        title:(NSString *)title
+                   titleColor:(UIColor *)titleColor
+                    titleFont:(CGFloat)titleFont
+                       target:(id)target
+                       action:(SEL)action;
 @end
--- UIBarButtonItem+LExtension.m
+++ UIBarButtonItem+LExtension.m
@@ -3,7 +3,7 @@
 //  UIBarButtonItem+LExtension
 //
 //  Created by Jimmy_Lee on 2016/12/19.
-//  Copyright © 2016年 jimmy_lee. All rights reserved.
+//  Copyright © 2016年 Jimmy_Lee. All rights reserved.
 //
 
 #import "UIBarButtonItem+LExtension.h"
@@ -11,23 +11,79 @@
 @implementation UIBarButtonItem (LExtension)
 
 /**
- * 创建带图片的barButtonItem
+ 创建带有图片的barButtonItem
+ 
+ @param image       默认图片
+ @param selectImage 选中图片
+ @param target      对象
+ @param action      点击事件
+ @return            创建实例对象
  */
-+ (instancetype)itemWithImage:(NSString *)image HeightImage:(NSString *)heightImage Target:(id)target action:(SEL)action{
++ (instancetype)itemWithImage:(NSString *)image
+                  selectImage:(NSString *)selectImage
+                       target:(id)target
+                       action:(SEL)action{
+    
     UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
     [button setBackgroundImage:[UIImage imageNamed:image] forState:UIControlStateNormal];
-    [button setBackgroundImage:[UIImage imageNamed:heightImage] forState:UIControlStateHighlighted];
+    [button setBackgroundImage:[UIImage imageNamed:selectImage] forState:UIControlStateHighlighted];
     [button sizeToFit];
     [button addTarget:target action:action forControlEvents:UIControlEventTouchUpInside];
     return [[self alloc]initWithCustomView:button];
 }
 
+
+
 /**
- * 创建只有文字的barButtonItem，并且可以设置文字颜色
+ 创建只有文字的barButtonItem，并且可以设置文字颜色
+ 
+ @param title       标题
+ @param titleColor  标题颜色
+ @param titleFont   标题字体大小
+ @param target      对象
+ @param action      点击事件
+ @return            创建实例对象
  */
-+ (instancetype)itemWithTitle:(NSString *)title TitleColor:(UIColor *)titleColor TitleFont:(CGFloat)titleFont Target:(id)target action:(SEL)action{
++ (instancetype)itemWithTitle:(NSString *)title
+                   titleColor:(UIColor *)titleColor
+                    titleFont:(CGFloat)titleFont
+                       target:(id)target
+                       action:(SEL)action{
     
     UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
+    [button setTitle:title forState:UIControlStateNormal];
+    [button setTitleColor:titleColor forState:UIControlStateNormal];
+    [button setTitleColor:[UIColor redColor] forState:UIControlStateHighlighted];
+    button.titleLabel.font = [UIFont systemFontOfSize:titleFont];
+    [button sizeToFit];
+    [button addTarget:target action:action forControlEvents:UIControlEventTouchUpInside];
+    return [[self alloc]initWithCustomView:button];
+}
+
+
+/**
+ 创建带有图片和文字的barButtonItem
+ 
+ @param image       默认图片
+ @param selectImage 选中时的图片
+ @param title       标题
+ @param titleColor  标题颜色
+ @param titleFont   标题字体大小
+ @param target      对象
+ @param action      点击事件
+ @return            创建实例对象
+ */
++ (instancetype)itemWithImage:(NSString *)image
+                  selectImage:(NSString *)selectImage
+                        title:(NSString *)title
+                   titleColor:(UIColor *)titleColor
+                    titleFont:(CGFloat)titleFont
+                       target:(id)target
+                       action:(SEL)action{
+    
+    UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
+    [button setImage:[UIImage imageNamed:image] forState:UIControlStateNormal];
+    [button setImage:[UIImage imageNamed:selectImage] forState:UIControlStateHighlighted];
     [button setTitle:title forState:UIControlStateNormal];
     [button setTitleColor:titleColor forState:UIControlStateNormal];
     [button setTitleColor:[UIColor redColor] forState:UIControlStateHighlighted];
